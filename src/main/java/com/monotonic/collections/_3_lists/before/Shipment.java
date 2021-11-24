8bc07c9a1a717d6ee11a9d01f082106f6524f811
cruby9339@gmail.com <christian.ruby72@gmail.com>
2021-11-24 12:25:59 -0600

    adds list lesson

--- src/main/java/com/monotonic/collections/_3_lists/before/Shipment.java
+++ src/main/java/com/monotonic/collections/_3_lists/before/Shipment.java
@@ -3,42 +3,79 @@ package com.monotonic.collections._3_lists.before;
 import com.monotonic.collections.common.Product;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
 public class Shipment implements Iterable<Product>
 {
     private static final int LIGHT_VAN_MAX_WEIGHT = 20;
+    private static final int MISSING_PRODUCT = -1;
+
+    private final List<Product> products = new ArrayList<>();
+
+    private List<Product> lightVanProducts;
+    private List<Product> heavyVanProducts;
 
     public void add(Product product)
     {
-        // TODO
+       products.add(product);
     }
+
 
     public boolean replace(Product oldProduct, Product newProduct)
     {
-        // TODO
+       int position = products.indexOf(oldProduct);
+       if(position == MISSING_PRODUCT){
+           return false;
+       } else {
 
-        return true;
+           products.set(position, newProduct);
+           return true;
+       }
     }
+
 
     public void prepare()
     {
-        // TODO
+        //sort the product list
+        Collections.sort(products, Product.BY_WEIGHT);
+
+
+        //find the split point
+        int splitPoint = findSplitPoint();
+
+
+        //create two subviews of the list
+       lightVanProducts = products.subList(0, splitPoint);
+       heavyVanProducts = products.subList(splitPoint, products.size());
     }
+
+    private int findSplitPoint(){
+
+        int size = products.size();
+        for(int i = 0; i < size; i++){
+           Product product = products.get(i);
+           if (product.getWeight() > LIGHT_VAN_MAX_WEIGHT){
+               return i;
+           }
+        }
+        return 0;
+    }
+
 
     public List<Product> getHeavyVanProducts()
     {
-        return null;
+        return heavyVanProducts;
     }
 
     public List<Product> getLightVanProducts()
     {
-        return null;
+        return lightVanProducts;
     }
 
     public Iterator<Product> iterator()
     {
-        return null;
+        return products.iterator();
     }
 }
